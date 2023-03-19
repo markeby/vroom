fafe661b45eb4ebcc54735a71d5581be186f9a6c
Doug Reed <dougr33d@gmail.com>
2023-03-18 22:50:17 -0500

    gtkwave tcl file

--- wav.tcl
+++ wav.tcl
@@ -1,31 +1,39 @@
 gtkwave::/Edit/Insert_Comment "Vroom"
 
 # Helper procs
-proc addSignalGroup {name path sig_list} {
-    set sigs_full [list]
-    foreach s $sig_list {
-        lappend sigs_full [format "%s.%s" $path $s]
+proc prefixAll {prefix xs} {
+    set finals [list]
+    foreach x $xs {
+        lappend finals [format "%s%s" $prefix $x]
     }
-    puts $sigs_full
+    return $finals
+}
 
-    gtkwave::addSignalsFromList $sigs_full
-    gtkwave::highlightSignalsFromList $sigs_full
+proc addSignalGroup {name sigs_list} {
+    gtkwave::addSignalsFromList $sigs_list
+    groupSignals $name $sigs_list
+}
+
+proc groupSignals {name sigs_list} {
+    gtkwave::highlightSignalsFromList $sigs_list
     gtkwave::/Edit/Create_Group $name
     gtkwave::/Edit/UnHighlight_All
 }
 
-#proc prefixAll {prefix xs} {
-#    set finals [list]
-#    foreach x $xs {
-#        lappend finals [format "%s%s" $prefix $x]
-#    }
-#    return $finals
-#}
+# Core signals
+set sigs      [list clk reset]
+set sigs      [prefixAll "top.core." $sigs]
+addSignalGroup "Core" $sigs
+puts $sigs
 
 # Fetch signals
-set sigs      [list instr_de0.opcode]
-addSignalGroup "Fetch" "top.core" $sigs
+set sigs      [list instr_de0.opcode valid_de0]
+set fe_sigs   [prefixAll "top.core." $sigs]
+addSignalGroup "Fetch" $fe_sigs
 
 # Decode signals
-set sigs      [list uinstr_de0.opcode]
-addSignalGroup "Decode" "top.core.decode" $sigs
+set uinstr_fields [prefixAll "uinstr_de0." [list funct imm32 opcode valid]]
+set sigs      [list {*}$uinstr_fields uinstr_de0.dst.oreg]
+set de_sigs   [prefixAll "top.core.decode." $sigs]
+addSignalGroup "Decode" $de_sigs
+

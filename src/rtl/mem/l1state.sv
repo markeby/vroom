36e068813a2b1fc25a13073ec3ac0946e29d19b1
Doug Reed <dougr33d@gmail.com>
2024-09-12 05:44:35 -0500

    preloading

---
+++ src/rtl/mem/l1state.sv
@@ -0,0 +1,65 @@
+`ifndef __L1STATE_SV
+`define __L1STATE_SV
+
+`include "instr.pkg"
+`include "vroom_macros.sv"
+`include "rob_defs.pkg"
+`include "mem_common.pkg"
+`include "mem_defs.pkg"
+
+module l1state
+    import instr::*, instr_decode::*, common::*, rob_defs::*, mem_defs::*, mem_common::*;
+(
+    input  logic              clk,
+    input  logic              reset,
+
+    input  logic              state_rd_en_mm1,
+    input  logic              state_wr_en_mm1,
+    input  t_l1_set_addr      set_addr_mm1,
+    input  t_mesi             state_wr_state_mm1,
+    input  t_l1_way           state_wr_way_mm1,
+
+    output t_mesi             state_rd_ways_mm2[L1_NUM_WAYS-1:0]
+);
+
+//
+// Nets
+//
+
+t_mesi state_array [L1_NUM_SETS-1:0][L1_NUM_WAYS-1:0];
+
+//
+// Logic
+//
+
+always_ff @(posedge clk) begin
+    if (state_wr_en_mm1) begin
+        state_array[set_addr_mm1][state_wr_way_mm1] <= state_wr_state_mm1;
+    end
+end
+
+`DFF(state_rd_ways_mm2, state_array[set_addr_mm1], clk)
+
+//
+// Debug
+//
+
+`ifdef SIMULATION
+// always @(posedge clk) begin
+//     if (iss_mm0) begin
+//         `INFO(("unit:MM %s", describe_uinstr(iss_pkt_mm0.uinstr)))
+//     end
+// end
+`endif
+
+    /*
+`ifdef ASSERT
+`VASSERT(a_illegal_format, uinstr_de1.valid, uinstr_de1.ifmt inside {RV_FMT_I,RV_FMT_R}, $sformatf("Unsupported instr fmt: %s", uinstr_de1.ifmt.name()))
+`endif
+    */
+
+endmodule
+
+`endif // __L1STATE_SV
+
+
---
+++ src/rtl/sim/preload.sv
@@ -0,0 +1,252 @@
+`ifndef __PRELOAD_SV
+`define __PRELOAD_SV
+
+`include "instr.pkg"
+`include "asm.pkg"
+`include "mem_common.pkg"
+`include "vroom_macros.sv"
+
+module preload
+    import instr::*, asm::*, mem_common::*, common::*;
+(
+    input  logic      clk,
+    input  logic      reset
+);
+
+localparam IROM_SZ     = 128;
+localparam IROM_SZ_LG2 = $clog2(IROM_SZ);
+
+t_word IROM[IROM_SZ-1:0];
+
+//
+// ROM
+//
+
+/* verilator lint_off WIDTHEXPAND */
+initial begin
+    automatic int a=0;
+    for (int i=0; i<IROM_SZ; i++) begin
+        IROM[i] = rvADDI(30,0,12'h666);
+    end
+
+    a=0;
+
+    // `define TEST_ALL_DEPS
+    // `define TEST_NO_DEPS
+    // `define TEST_ARITH
+    //`define TEST_LOGICAL
+    //`define TEST_SLT
+    //`define TEST_BR_NT
+    //`define TEST_BR_T
+    //`define TEST_BEEF
+    `define TEST_LOAD
+
+    // IROM[a++] = rvADDI(1,1,12'h1);
+    // IROM[a++] = rvADDI(8,8,12'h1);
+    // IROM[a++] = rvADDI(8,8,12'h1);
+    // IROM[a++] = rvADDI(4,4,12'h1);
+    // IROM[a++] = rvADDI(2,2,12'h1);
+    // IROM[a++] = rvADDI(8,8,12'h1);
+    // IROM[a++] = rvADDI(4,4,12'h1);
+    // IROM[a++] = rvADDI(4,4,12'h1);
+    // IROM[a++] = rvADDI(8,8,12'h1);
+    // IROM[a++] = rvADDI(8,8,12'h1);
+    // IROM[a++] = rvADDI(8,8,12'h1);
+    // IROM[a++] = rvADDI(4,4,12'h1);
+    // IROM[a++] = rvADDI(8,8,12'h1);
+    // IROM[a++] = rvADDI(2,2,12'h1);
+    // IROM[a++] = rvADDI(8,8,12'h1);
+    // IROM[a++] = rvBNE(0,1,8);
+    // IROM[a++] = rvADDI(0,0,12'h42);
+    // IROM[a++] = rvADDI(1,1,32);
+
+    // IROM[a++] = rvXOR(1,1,1);
+    // IROM[a++] = rvXOR(2,2,2);
+    // IROM[a++] = rvXOR(3,3,3);
+    // IROM[a++] = rvXOR(4,4,4);
+    // IROM[a++] = rvXOR(5,5,5);
+
+    // IROM[a++] = rvXOR(1,1,1);
+    // IROM[a++] = rvXOR(2,1,1);
+    // IROM[a++] = rvXOR(3,2,2);
+    // IROM[a++] = rvXOR(4,3,3);
+    // IROM[a++] = rvXOR(5,4,4);
+    // IROM[a++] = rvXOR(6,5,5);
+    // IROM[a++] = rvXOR(7,6,6);
+    // IROM[a++] = rvXOR(8,7,7);
+    // IROM[a++] = rvXOR(9,8,8);
+
+    `ifdef TEST_LOAD
+        IROM[a++] = rvLB(9,10,12'h100);
+    `endif
+    `ifdef TEST_BR_NT
+        IROM[a++] = rvXOR(1,1,1);
+        IROM[a++] = rvADDI(1,1,32);
+        IROM[a++] = rvBEQ(0,1,8);
+        IROM[a++] = rvADDI(0,0,12'h42);
+        IROM[a++] = rvADDI(1,1,32);
+    `endif
+    `ifdef TEST_BR_T
+        IROM[a++] = rvXOR(1,1,1);
+        IROM[a++] = rvADDI(1,1,32);
+        IROM[a++] = rvBNE(0,1,8);
+        IROM[a++] = rvADDI(31,0,12'h666);
+        IROM[a++] = rvADDI(1,1,32);
+    `endif
+    `ifdef TEST_BEEF
+        IROM[a++] = rvXOR(1,1,1);
+        IROM[a++] = rvADDI(1,1,12'hDE); IROM[a++] = rvSLLI(1,1,6'd8);
+        IROM[a++] = rvADDI(1,1,12'hAD); IROM[a++] = rvSLLI(1,1,6'd8);
+        IROM[a++] = rvADDI(1,1,12'hBE); IROM[a++] = rvSLLI(1,1,6'd8);
+        IROM[a++] = rvADDI(1,1,12'hEF);
+        IROM[a++] = rvADDI(2,2,12'hCA); IROM[a++] = rvSLLI(2,2,6'd8);
+        IROM[a++] = rvADDI(2,2,12'hFE); IROM[a++] = rvSLLI(2,2,6'd8);
+        IROM[a++] = rvADDI(2,2,12'hBA); IROM[a++] = rvSLLI(2,2,6'd8);
+        IROM[a++] = rvADDI(2,2,12'hBE);
+    `endif
+    `ifdef TEST_ALL_DEPS
+        IROM[a++] = rvADDI(1,1,1);
+        IROM[a++] = rvADDI(1,1,1);
+        IROM[a++] = rvADDI(1,1,1);
+        IROM[a++] = rvADDI(1,1,1);
+        IROM[a++] = rvADDI(1,1,1);
+    `endif // TEST_ALL_DEPS
+    `ifdef TEST_NO_DEPS
+        IROM[a++] = rvADDI(1,1,1);
+        IROM[a++] = rvXORI(2,1,1);
+        IROM[a++] = rvXORI(3,1,1);
+        IROM[a++] = rvXORI(4,1,1);
+        IROM[a++] = rvXORI(5,1,1);
+        IROM[a++] = rvXORI(6,1,1);
+        IROM[a++] = rvXORI(7,1,1);
+    `endif //TEST_NO_DEPS
+    `ifdef TEST_ARITH
+        IROM[a++] = rvADDI(1,1,12'h123);
+        IROM[a++] = rvADDI(2,1,12'h123);
+        IROM[a++] = rvADD(3,1,2);
+        IROM[a++] = rvSUB(16,3,1);
+    `endif //TEST_ARITH
+    `ifdef TEST_LOGICAL
+        IROM[a++] = rvXOR(2,2,2);
+        IROM[a++] = rvADDI(1,0,12'h111);
+        IROM[a++] = rvADDI(17,0,12'h654);
+        IROM[a++] = rvXORI(18,16,12'hfff);
+        IROM[a++] = rvSRAI(20,18,6'h1);
+        IROM[a++] = rvSRLI(21,18,6'h1);
+        IROM[a++] = rvXOR(22,20,21);
+    `endif //TEST_LOGICAL
+    `ifdef TEST_SLT
+        IROM[a++] = rvXOR(1,1,1);
+        IROM[a++] = rvADDI(2,1,12'h50);
+        IROM[a++] = rvADDI(3,1,12'h4f);
+        IROM[a++] = rvADDI(4,1,12'h50);
+        IROM[a++] = rvADDI(5,1,12'h51);
+        IROM[a++] = rvSUB(6,1,5);
+        IROM[a++] = rvADDI(7,6,12'h1);
+        IROM[a++] = rvADDI(8,6,12'hfff);
+
+        IROM[a++] = rvSLT(12,1,2);
+        IROM[a++] = rvSLT(13,1,3);
+        IROM[a++] = rvSLT(14,1,4);
+        IROM[a++] = rvSLT(15,1,5);
+        IROM[a++] = rvSLT(16,1,6);
+        IROM[a++] = rvSLT(17,1,7);
+        IROM[a++] = rvSLT(18,1,8);
+
+        IROM[a++] = rvSLTU(12,1,2);
+        IROM[a++] = rvSLTU(13,1,3);
+        IROM[a++] = rvSLTU(14,1,4);
+        IROM[a++] = rvSLTU(15,1,5);
+        IROM[a++] = rvSLTU(16,1,6);
+        IROM[a++] = rvSLTU(17,1,7);
+        IROM[a++] = rvSLTU(18,1,8);
+    `endif //TEST_SLT
+
+    // IROM[a++] = rvADDI(6,0,12'h666); // magic to end test
+    // IROM[a++] = rvXOR(0,0,0);
+    // IROM[a++] = rvXOR(0,0,0);
+    // IROM[a++] = rvXOR(0,0,0);
+    IROM[a++] = rvEBREAK();
+
+    for (int i=0; i<a; i++) begin
+        `MEMLOG(("unit:IROM pa:%08h / %08h", i, IROM[i]))
+    end
+
+    // Copy contents of IROM into L2
+    // $display("a: %d", a);
+    for (t_paddr byte_addr=0; byte_addr < IROM_SZ*4; byte_addr += 64) begin
+        t_cl cl;
+        for (int w=0; w<CL_SZ_BYTES/4; w++) begin
+            cl.W[w] = IROM[byte_addr[IROM_SZ_LG2+1:2] + w];
+        end
+        preload_l2_cl(cl, byte_addr, 1);
+    end
+end
+/* verilator lint_on WIDTHEXPAND */
+
+function automatic void preload_l2_byte(t_byte b, t_paddr byte_addr, int display=0);
+    // $display("BYTE PRELOAD %0h %0h", byte_addr, b);
+    core.l2.MEMORY[byte_addr] = b;
+endfunction
+
+function automatic void preload_l2_word(t_word w, t_paddr byte_addr, int display=0);
+    if (byte_addr[1:0] != 0) begin
+        $error("preload_l2_word with misaligned addr");
+    end
+    for (logic[2:0] b=0; b<4; b++) begin
+        preload_l2_byte(w[8*b +: 8], byte_addr + t_paddr'(b), 0);
+    end
+endfunction
+
+function automatic t_word get_l2_word(t_paddr byte_addr);
+    logic[3:0][7:0] w;
+    w[0] = core.l2.MEMORY[byte_addr + 0];
+    w[1] = core.l2.MEMORY[byte_addr + 1];
+    w[2] = core.l2.MEMORY[byte_addr + 2];
+    w[3] = core.l2.MEMORY[byte_addr + 3];
+    get_l2_word = t_word'(w);
+endfunction
+
+function automatic void preload_l2_cl(t_cl c, t_paddr byte_addr, int display=0);
+    if (byte_addr[5:0] != 0) begin
+        $error("preload_l2_cl with misaligned addr");
+    end
+     // $display("PRELOAD %0h %0h", byte_addr, c);
+    for (t_paddr b=0; b<64; b++) begin
+        preload_l2_byte(c.B[b], byte_addr + b, 0);
+    end
+
+    // if (display) begin
+        `MEMLOG(("unit:PRELOAD pa:%08h / %016h %016h %016h %016h  %016h %016h %016h %016h ",
+            byte_addr,
+            {get_l2_word(byte_addr + 60), get_l2_word(byte_addr + 56)},
+            {get_l2_word(byte_addr + 52), get_l2_word(byte_addr + 48)},
+            {get_l2_word(byte_addr + 44), get_l2_word(byte_addr + 40)},
+            {get_l2_word(byte_addr + 36), get_l2_word(byte_addr + 32)},
+            {get_l2_word(byte_addr + 28), get_l2_word(byte_addr + 24)},
+            {get_l2_word(byte_addr + 20), get_l2_word(byte_addr + 16)},
+            {get_l2_word(byte_addr + 12), get_l2_word(byte_addr +  8)},
+            {get_l2_word(byte_addr +  4), get_l2_word(byte_addr     )}))
+    // end
+endfunction
+
+//
+// Displays
+//
+
+`ifdef SIMULATION
+// always @(posedge clk) begin
+//     if (~reset & fb_ic_req_nnn.valid) begin
+//         `MEMLOG(("unit:IC type:fb_ic_req_nnn id:%d addr:%h", fb_ic_req_nnn.id, fb_ic_req_nnn.addr))
+//     end
+//     if (~reset & ic_fb_rsp_nnn.valid) begin
+//         `MEMLOG(("unit:IC type:ic_fb_rsp_nnn id:%d data:%h", ic_fb_rsp_nnn.id, ic_fb_rsp_nnn.data))
+//     end
+// end
+`endif
+
+endmodule
+
+`endif // __PRELOAD_SV
+
+
